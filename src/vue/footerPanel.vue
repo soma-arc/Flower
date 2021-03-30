b145d71a8c386ff56fe4f1482c2685d52d3a05dc
soma_arc <somaarcr@gmail.com>
2021-03-30 12:55:55 +0900

    Add link to YouTube

--- src/vue/footerPanel.vue
+++ src/vue/footerPanel.vue
@@ -5,6 +5,10 @@
          target="_blank">
           <img src="../img/GitHub-Mark-32px.png" class="imgLink"/>
       </a>
+      <a id="link" href="https://youtu.be/FWp-eF5gz5o"
+         target="_blank">
+          Introductory video on YouTube
+      </a>
     </div>
     <div>
       Created by soma_arc
@@ -40,5 +44,28 @@ footer {
     justify-content: center;
     align-items: center;
     padding-left: 10px;
+}
+
+#link {
+    align-items: center;
+    justify-content: center;
+    padding-top: 20px;
+    padding-right: 5px;
+    padding-left: 5px;
+    margin-left: 5px;
+    border-right-style: ridge;
+    border-left-style: ridge;
+    border-right-color: gray;
+    border-left-color: gray;
+    text-decoration: none;
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -o-user-select: none;
+}
+
+#link:hover{
+    background-color: PaleTurquoise;
 }
 </style>
